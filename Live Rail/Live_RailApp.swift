8153fcdbff5e159e309121638da0d633306654e3
jap.thompson <141432257+johnapt7@users.noreply.github.com>
2026-04-26 23:04:09 +0100

    Initial Commit

---
+++ Live Rail/Live_RailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_RailApp.swift
+//  Live Rail
+//
+//  Created by John Thompson on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Live_RailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
